0cc3a44f29ea22f31719859374078eefea46adda
wagagt <wagagt@gmail.com>
2015-10-19 06:50:35 +0000

    Add layout to login. Fix menu options.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">Bienvenido</div>
 
 				<div class="panel-body">
-					You are logged in!
+					Elija una opción.
 				</div>
 			</div>
 		</div>
