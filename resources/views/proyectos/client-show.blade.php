8687503d32fcd4e0220bebe354dfd3c2e747dda5
wagagt <wagagt@gmail.com>
2015-12-12 14:11:08 +0000

    add modal html

--- resources/views/proyectos/client-show.blade.php
+++ resources/views/proyectos/client-show.blade.php
@@ -15,7 +15,7 @@
         <br>
         <div class="col-md-6">
             @if ($isAdmin=="true")
-                <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#commentModal">
                     Agregar Comentarios
                 </button>
             @endif
@@ -29,9 +29,31 @@
             Comentarios para proyecto:  {!! $proyecto->nombre !!}
         </div>
     </div>
-
-    
 </div>
+
+@if ($isAdmin=="true")
+    <!-- MODAL -->
+    <div class="modal fade in" id="commentModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: block; padding-right: 23px;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                    Lorem ipsum .
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- MODAL -->        
+@endif
 @endsection
 
 
