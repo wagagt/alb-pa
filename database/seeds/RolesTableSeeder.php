7f3f274f2d60c67f4d63fbd3d4c5d7aab1886fe2
wagagt <wagagt@gmail.com>
2015-12-12 12:47:54 +0000

    Add comments on seeders.

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -7,8 +7,8 @@ class RolesTableSeeder extends Seeder {
 public function run()
  {
     $rol = [
-     ['descripcion' => 'Admin', 'created_at' => new DateTime],
-     ['descripcion' => 'Usuario', 'created_at' => new DateTime]
+     ['descripcion' => 'Admin', 'created_at' => new DateTime],  // 1 = Admin
+     ['descripcion' => 'Usuario', 'created_at' => new DateTime] // 2 = Usuario
      ];
     DB::table('roles')->insert($rol);
  }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -8,15 +8,15 @@ public function run()
  {
     $user = 
     [
-     ['name' => 'wagagt', 'contact_fname' => 'Wilver', 'contact_lname' => 'Gonzalez', 
+     ['name' => 'wagagt', 'contact_fname' => 'Wilver', 'contact_lname' => 'Gonzalez',  // wagagt@gmail.com = Admin
       'email' => 'wagagt@gmail.com', 'password' => Hash::make("waga15"), 
       'id_rol'=>'1', 'id_cliente'=>'1', 'created_at' => new DateTime
      ],
-     ['name' => 'fabiogt', 'contact_fname' => 'Fabio', 'contact_lname' => 'Gonzalez', 
+     ['name' => 'fabiogt', 'contact_fname' => 'Fabio', 'contact_lname' => 'Gonzalez', // usuario
       'email' => 'fabiogt@gmail.com', 'password' => Hash::make("fabio15"), 
       'id_rol'=>'2', 'id_cliente'=>'2', 'created_at' => new DateTime
      ],
-     ['name' => 'diegogt', 'contact_fname' => 'Diego', 'contact_lname' => 'Gonzalez', 
+     ['name' => 'diegogt', 'contact_fname' => 'Diego', 'contact_lname' => 'Gonzalez', // usuario
       'email' => 'diegogt@gmail.com', 'password' => Hash::make("fabio15"), 
       'id_rol'=>'2', 'id_cliente'=>'3', 'created_at' => new DateTime
      ]
