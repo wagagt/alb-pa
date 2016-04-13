a3232738228ff990139bd04c243b53e2deb57890
Axel Sarceño <axelsarceno.delta@gmail.com>
2016-04-12 23:52:10 -0600

    add protected username in AuthController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
     | a simple trait to add these behaviors. Why don't you explore it?
     |
     */
-
+    protected $username = 'usuario';
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     /**
