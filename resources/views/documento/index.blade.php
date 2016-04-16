c45267ba258d4a85de7d95caa38fb9e42789b9b4
Axel Sarceño <axelsarceno.delta@gmail.com>
2016-04-15 19:03:01 -0600

    fix correction relation and view documentos

--- resources/views/documento/index.blade.php
+++ resources/views/documento/index.blade.php
@@ -24,14 +24,14 @@
 
           <div class="box-body">
             <div class="col-md-12 text-left"><a href="{{route('documento.create')}}" class="btn btn-primary"><i class="fa fa-briefcase"></i> Crear Documentos </a>  </div>
-        
+
         <table class="table table-hover">
             <thead>
                 <td><strong>NOMBRE</strong></td>
                 <td><strong>TIPO DOC.</strong></td>
                 <td><strong>DESDE</strong></td>
                 <td><strong>HASTA</strong></td>
-                <td><strong>CREADO POR</strong></td>
+                <td><strong>EDIFICIO</strong></td>
                 <td><strong>ACCIONES</strong></td>
             </thead>
             <tbody>
@@ -41,11 +41,10 @@
                     <td>{{$value->tipo_documento->descripcion}}</td>
                     <td>{{$value->fecha_del}}</td>
                     <td>{{$value->fecha_al}}</td>
-                    <td>{{$value->user->usuario}}</td>
+                    <td>{{$value->torre->nombre}}</td>
                     <td>
-                        <td>
                     <a href="{{ route('documento.edit', $value->id) }}" class="btn btn-warning" title="Editar">
-                      <i class="fa fa-pencil-square-o"></i></a> 
+                      <i class="fa fa-pencil-square-o"></i></a>
                     <a href="{{ route('documento.destroy', $value->id) }}"
                       class="btn btn-danger" title="Elimiar" onclick="return confirm('¿Seguro que desea eliminar el registro?')">
                       <i class="fa fa-trash"></i>
