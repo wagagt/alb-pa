65f4618dbac058eae632a93341c3cd5cdc20b9eb
Axel Sarceño <axelsarceno.delta@gmail.com>
2016-05-07 20:50:21 -0600

    modify footer and aside menu, delete documentos from option menu

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -2,8 +2,10 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Anything you want
+      Systems, Web Sites and App Android &copy;
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="#"> Plataforma desarrollada por</a></strong>- <a href="http://www.hosannaweb.com" target="_blank">Hosanna Web &reg;</a> - Para ALB Panamá Todos los derechos reservados.
+    <strong>Copyright &copy;
+ 2016 <a href="#"> Plataforma desarrollada por</a></strong>- <a href="http://www.hosannaweb.com" target="_blank">Hosanna Web
+</a>&reg; - Para ALB Panamá Todos los derechos reservados.
   </footer>
