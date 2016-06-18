7ee8f1183c80be07edddf68094c38ea832ecb720
WagaGt <wagagt@gmail.com>
2016-06-18 15:32:50 -0600

    Fix: change url to load a avatar image.

--- resources/views/propietario/partials/header.blade.php
+++ resources/views/propietario/partials/header.blade.php
@@ -33,7 +33,7 @@
                     <a href="#">
                       <div class="pull-left">
                         <!-- User Image -->
-                        <img src="{{asset('ui/images/avataruser.png')}}" class="img-circle" alt="User Image">
+                        <img src="{{asset('/ui/images/avataruser.png')}}" class="img-circle" alt="User Image">
                       </div>
                       <!-- Message title and timestamp -->
                       <h4>
@@ -117,7 +117,7 @@
             <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <!-- The user image in the navbar-->
-              <img src="ui/images/avataruser.png" class="user-image" alt="User Image">
+              <img src="/ui/images/avataruser.png" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
               <span class="hidden-xs">{{Auth::user()->name}}</span>
             </a>
