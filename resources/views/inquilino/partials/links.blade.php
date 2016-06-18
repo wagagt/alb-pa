ed20425989da33ba36790134dd73b993c7306314
WagaGt <wagagt@gmail.com>
2016-06-18 15:32:27 -0600

    Fix route to load a min.css file.

--- resources/views/inquilino/partials/links.blade.php
+++ resources/views/inquilino/partials/links.blade.php
@@ -1,6 +1,6 @@
   <link href="{{ asset('bower_components/AdminLTE/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
   <link href="{{ asset('bower_components/AdminLTE/bootstrap/css/font-awesome.css') }}" rel="stylesheet" type="text/css">
-  <link href="{{ asset('bower_components/AdminLTE/bootstrap/css/font-awesome.mini.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('bower_components/AdminLTE/bootstrap/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
   <link href="{{ asset('bower_components/AdminLTE/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css">
   <link href="{{ asset('bower_components/AdminLTE/dist/css/skins/skin-blue.min.css') }}" rel="stylesheet" type="text/css">
