e55a741e37239f507f810fc969f143704f4e5850
wagadelta <wilvergonzalez.delta@gmail.com>
2016-07-19 04:03:44 +0000

    feature: Add Image Intervention (plugin: to handle avatar images ).

--- config/app.php
+++ config/app.php
@@ -162,7 +162,8 @@ return [
         Maatwebsite\Excel\ExcelServiceProvider::class,
         //log
         Spatie\Activitylog\ActivitylogServiceProvider::class,
-
+    
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -211,6 +212,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
 
 
