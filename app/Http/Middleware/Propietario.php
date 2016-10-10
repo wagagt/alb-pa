377d55d435e62ba921bac66b10543b49b5f63d56
wagadelta <wilvergonzalez.delta@gmail.com>
2016-10-10 02:21:55 +0000

    Fix: Middleware propietario using isPropietario() ( method on byself )

--- app/Http/Middleware/Propietario.php
+++ app/Http/Middleware/Propietario.php
@@ -18,12 +18,9 @@ class Propietario
 
     public function handle($request, Closure $next)
     {
-        if($this->auth->user()->tipo= 'propietario'){
-            Activity::log('Logged in');
-
-
-            if ( $this->auth->user()->password != '$2y$10$cCOppp.HKq6h2BbfAMvc5eGbrA9ED/J97.4BEOLEl/OoAEd463ioi' ){
-
+        if($this->auth->user()->isPropietario()){
+            // TO DO: save logued in activity on log....Activity::log('Logged in');
+            if ( $this->auth->user()->password == '$2y$10$cCOppp.HKq6h2BbfAMvc5eGbrA9ED/J97.4BEOLEl/OoAEd463ioi' ){
                 return view('propietario.dash');
             }
         }
