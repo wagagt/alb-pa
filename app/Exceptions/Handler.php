b210351deffe519fd71bb5d484a4a58e1ca8c334
vagrant <vagrant@homestead>
2016-12-20 06:50:45 +0000

    add response 404 view

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,10 @@ class Handler extends ExceptionHandler
             $e = new NotFoundHttpException($e->getMessage(), $e);
         }
 
+        if ($e instanceof NotFoundHttpException) {
+            return response(view('errors.404'),404);
+        }
+
         return parent::render($request, $e);
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceso denegado</title>
+    <link rel="stylesheet" href="{{asset('bower_components/AdminLTE/bootstrap/css/bootstrap.min.css')}}" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="{{asset('bower_components/AdminLTE/bootstrap/css/font-awesome.min.css')}}" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+<div class="text-center">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-warning">
+            <div class="panel-heading">
+                @include('flash::message')
+                <div class="panel-title">Pagina no encontrada</div>
+            </div>
+            <div class="panel-body">
+                <img class="img-responsive center-block" src="{{asset('images/secure.png')}}"></div>
+            <hr/>
+            <strong class="text-center">
+                <p class="text-center"> Oops!! lo sentimos la pagina no ha sido encontrada</p>
+                <p>  <a href="{{ route('admin.auth.login')}}" class="btn btn-warning"><i class="fa fa-times"></i> volver al inicio</a></p>
+            </strong>
+
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
